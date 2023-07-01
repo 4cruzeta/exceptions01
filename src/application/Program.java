b05464db4b925e1d7f3c61390fe62a4f229932f6
4cruzeta <4cruzetam@gmail.com>
2023-07-01 07:42:13 -0300

    Funciona mas é uma solução ruim.

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,15 +36,19 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
+			/*
+			 * Essa sequência de validação fica melhor colocada na classe
+			 * a que pertence, delegação. Solução que será implementada 
+			 * a seguir. Quando acontecer erro retorna string. Tipo de
+			 * solução comum em linguagens antigas.
+			 */
+			
+
+				String error = reservation.updateDates(checkIn, checkOut);
+				if (error !=null) {
+					System.out.println("Erro na reserva: " + error);
+				}
+				else {
 				System.out.println("Reservation: " + reservation);
 			}
 		}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -40,9 +40,20 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) { 
+		
+		// De void para String para retornar mensagem de erro.
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null; // significa que não houve erro.
 	}
 
 	@Override
